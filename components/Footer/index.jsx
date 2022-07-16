e2e28dd03cbc72ae9de5b27ffda851121f4109b8
torik135 <riqibrekele@gmail.com>
2022-07-16 18:40:16 +0700

    add footer content

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+
+import { FaSortDown, FaSortUp } from 'react-icons/fa';
+
 const Footer = () => {
+  const [foot, setFoot] = useState(true);
+
   return (
-    <footer className='footer'>
-      <div className='footer_content'>Footer</div>
+    <footer
+      className={foot ? 'footer up' : 'footer'}
+      onClick={() => setFoot(!foot)}
+    >
+      <div className='footer_content'>
+        <div className='footer_icon'>
+          <i>{foot ? <FaSortDown /> : <FaSortUp />}</i>
+        </div>
+        <div className='copyright'>torik135 | 2022</div>
+      </div>
     </footer>
   );
 };
